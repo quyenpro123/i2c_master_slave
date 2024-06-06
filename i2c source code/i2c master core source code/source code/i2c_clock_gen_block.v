3fba7abd55c2468309ce0bcf4e57bad7a024fcbe
Hoang Van Quyen <101342563+quyenpro123@users.noreply.github.com>
2024-06-06 09:20:28 +0700

    Update i2c_clock_gen_block.v

--- i2c source code/i2c master core source code/source code/i2c_clock_gen_block.v
+++ i2c source code/i2c master core source code/source code/i2c_clock_gen_block.v
@@ -8,20 +8,20 @@ module i2c_clock_gen_block(
     reg temp_scl_o                                                                          ; // temp variable
     assign scl_o = temp_scl_o                                                               ; // update sda output clock
 
-    //count i2c core clock, scl get positive edge when counter detect edge = 2 * prescaler - 1, opposite, counter detect edge = prescaler - 1 
-    always @(posedge i2c_core_clock_i) 
+    //count i2c core clock, scl get positive edge when counter detect edge = 2 * prescaler - 1, opposite, counter detect edge = prescaler - 1
+    always @(posedge i2c_core_clock_i, negedge reset_bit_n_i) 
     begin
         if (~reset_bit_n_i)
-            counter_detect_edge_o <= (2 * prescaler_i - 1)                                  ;
+            counter_detect_edge_o <= 0                                                      ;
         else
-            if (counter_detect_edge_o == 0)   
-                counter_detect_edge_o <= (2 * prescaler_i - 1)                              ;
+            if (counter_detect_edge_o == (2 * prescaler_i - 1))   
+                counter_detect_edge_o <= 0                                                  ;
             else
-                counter_detect_edge_o <= counter_detect_edge_o - 1                          ;
+                counter_detect_edge_o <= counter_detect_edge_o + 1                          ;
     end
 
     //gen scl clock
-    always @(posedge i2c_core_clock_i) 
+    always @(posedge i2c_core_clock_i, negedge reset_bit_n_i) 
     begin
         if (~reset_bit_n_i)
             temp_scl_o <= 1                                                                 ;
