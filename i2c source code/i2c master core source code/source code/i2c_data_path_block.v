2533eaa46dbf82593889bf77aa7fb9d26b746afd
Hoang Van Quyen <101342563+quyenpro123@users.noreply.github.com>
2024-06-06 09:20:52 +0700

    Update i2c_data_path_block.v

--- i2c source code/i2c master core source code/source code/i2c_data_path_block.v
+++ i2c source code/i2c master core source code/source code/i2c_data_path_block.v
@@ -25,22 +25,23 @@ module i2c_data_path_block (
     assign sda_o = temp_sda_o                                                                   ; //update sda
     
     //handle counter data, ack
-    always @(posedge i2c_core_clock_i) 
+    always @(posedge i2c_core_clock_i, negedge reset_bit_n_i) 
     begin
         if (~reset_bit_n_i)
-            counter_data_ack_o <= 9                                                             ;
-        else 
-            if (counter_data_ack_o == 0)
-                counter_data_ack_o <= 9                                                         ;
-            if (counter_detect_edge_i == (2 * prescaler_i - 1) &&                                //when posedge of scl
+            counter_data_ack_o <= 0                                                             ;
+        else  
+        begin
+            if (counter_data_ack_o == 9)
+                counter_data_ack_o <= 0                                                         ;
+            if (counter_detect_edge_i == prescaler_i &&                                //when posedge of scl
                 (write_addr_cnt_i || write_ack_cnt_i ||
                 read_data_cnt_i || write_data_cnt_i || read_ack_cnt_i))
-                counter_data_ack_o <= counter_data_ack_o - 1                                    ;
-            
+                counter_data_ack_o <= counter_data_ack_o + 1                                    ;
+       end   
     end
 
     //handle sda when datapath write
-    always @(posedge i2c_core_clock_i) 
+    always @(posedge i2c_core_clock_i, negedge reset_bit_n_i) 
     begin
         if (~reset_bit_n_i)
             temp_sda_o <= 1                                                                     ;
@@ -48,13 +49,13 @@ module i2c_data_path_block (
             begin
                 if (start_cnt_i == 1)
                     temp_sda_o <= 0                                                             ;
-                else if (write_addr_cnt_i && counter_detect_edge_i == (prescaler_i - 1))        // write addr after negedge of scl 1 i2c core clock        
-                    temp_sda_o <= addr_rw_i[counter_data_ack_o - 2]                             ;
-                else if (write_data_cnt_i && counter_detect_edge_i == (prescaler_i - 1))        // write data after negedge of scl 1 i2c core clock            
-                    temp_sda_o <= data_i[counter_data_ack_o - 2]                                ;
-                else if (write_ack_cnt_i && counter_detect_edge_i == (prescaler_i - 1))         // write ack after negedge of scl 1 i2c core clock
+                else if (write_addr_cnt_i && counter_detect_edge_i == 1)        // write addr after negedge of scl 1 i2c core clock
+                    temp_sda_o <= addr_rw_i[7 - counter_data_ack_o]                             ;
+                else if (write_data_cnt_i && counter_detect_edge_i == 1)        // write data after negedge of scl 1 i2c core clock
+                    temp_sda_o <= data_i[7 - counter_data_ack_o]                                ;
+                else if (write_ack_cnt_i && counter_detect_edge_i == 1)         // write ack after negedge of scl 1 i2c core clock
                     temp_sda_o <= ack_bit_i                                                     ;
-                else if (stop_cnt_i && counter_detect_edge_i == (prescaler_i - 1))
+                else if (stop_cnt_i && counter_detect_edge_i == 1)
                     temp_sda_o <= 0                                                             ;
                 else if (repeat_start_cnt_i)
                     if (counter_state_done_time_repeat_start_i > 1)
@@ -65,13 +66,13 @@ module i2c_data_path_block (
     end
 
     //handle when datapath read
-    always @(posedge i2c_core_clock_i) 
+    always @(posedge i2c_core_clock_i, negedge reset_bit_n_i) 
     begin
         if (~reset_bit_n_i)
             data_o <= 0                                                                         ;
         else
-            if (read_data_cnt_i && counter_detect_edge_i == (2 * prescaler_i - 1))
-                data_o[counter_data_ack_o - 2] <= sda_i                                         ;
+            if (read_data_cnt_i && counter_detect_edge_i == prescaler_i)
+                data_o[7 - counter_data_ack_o] <= sda_i                                         ;
     end
 
 
